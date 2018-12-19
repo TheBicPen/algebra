78c49565d0004e6bf0b484a76ab9a6d719ea5d77
The Bic Pen <mashkal2000@gmail.com>
2018-12-18 22:25:07 -0500

    Removed form1. Turned project into class library. Added project for interfacing with the classes.

---
+++ Algebra/Algebra/Expression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algebra
+{
+    class Expression
+    {
+        private string ExpressionString = "";
+
+        public int Evaluate()
+        {
+            return 0;
+        }
+
+       
+
+    }
+}
